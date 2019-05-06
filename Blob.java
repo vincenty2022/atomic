94142355cf7362dc02fc4a1cb853836dd82893b4
Leonardo Espinoza <leonmat26@gmail.com>
2019-05-06 18:31:59 -0400

    What?

--- Blob.java
+++ Blob.java
@@ -19,7 +19,7 @@ public class Blob {
     private int mass = 0;
 
     // stores initial center of mass coordinates
-    private int[] cntrMass = {0,0};
+    private int[] cntrMass = { 0, 0 };
 
     // empty blob object
     public Blob() {
@@ -27,8 +27,8 @@ public class Blob {
 
     public void add(int x, int y) {
         // modifies center of gravity of the blob
-        cntrMass[0] = cntrMass[0] * mass + x / (mass+ 1);
-        cntrMass[1] = cntrMass[1] * mass + y / (mass+ 1);
+        cntrMass[0] = cntrMass[0] * mass + x / (mass + 1);
+        cntrMass[1] = cntrMass[1] * mass + y / (mass + 1);
 
         // creates new coordinate array
         int[] ptcoord = new int[2];
@@ -40,17 +40,17 @@ public class Blob {
         mass++;
     }
 
-    public int mass() {
-        return mass;
-    }
-
-    public double distanceTo(Blob that) {
-
-    }
-
-    public String toString() {
-
-    }
+    // public int mass() {
+    //     return mass;
+    // }
+    //
+    // public double distanceTo(Blob that) {
+    //
+    // }
+    //
+    // public String toString() {
+    //
+    // }
 
     public static void main(String[] args) {
 
